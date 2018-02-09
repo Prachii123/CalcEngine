2580f07b4024c6b6abf967622ce4fb65997bba8a
Prachi Ancharwadkar <pancharwadkar@evertz.com>
2018-02-09 19:30:08 +0000

    introducing exception handling

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -7,6 +7,9 @@ public class Main {
 //        useCalculatorBase();
 
         String[] statements = {
+                "add 1.0",
+                "add xx 25.0",
+                "addX 0.0 0.0",
                 "divide 100.0 50.0",   // 100.0 / 50.0 = 2.0
                 "add 25.0 92.0",       // 25.0 + 92.0 = 117.0
                 "subtract 225.0 17.0", // 225.0 - 17.0 = 108.0
@@ -15,8 +18,14 @@ public class Main {
 
         CalculateHelper helper = new CalculateHelper();
         for(String statement:statements) {
-            helper.process(statement);
-            System.out.println(helper);
+            try {
+                helper.process(statement);
+                System.out.println(helper);
+            } catch(InvalidStatementException e){
+                System.out.println(e.getMessage());
+                if(e.getCause() != null)
+                    System.out.println(" original exception: "+e.getCause().getMessage());
+            }
         }
 
 
